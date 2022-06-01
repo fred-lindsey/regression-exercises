2b4eaf1660745adc72e557301606081a99b0ef93
Fred Lindsey <fred.lindsey.92@gmail.com>
2022-06-01 16:31:46 -0500

    created scaling function, added to wrangle.py, completed exercises

--- split.py
+++ split.py
@@ -29,9 +29,9 @@ def train_validate_test_split(df, target, seed=123):
     The function returns, in this order, train, validate and test dataframes. 
     '''
     train_validate, test = train_test_split(df, test_size=0.2, 
-                                            random_state=seed, 
-                                            stratify=df[target])
+                                            random_state=seed) 
+                                            #stratify=df[target])
     train, validate = train_test_split(train_validate, test_size=0.3, 
-                                       random_state=seed,
-                                       stratify=train_validate[target])
+                                       random_state=seed)
+                                       #stratify=train_validate[target])
     return train, validate, test
--- wrangle.py
+++ wrangle.py
@@ -10,7 +10,9 @@
 # wrangle_zillow(): combination of the two above functions that produces a 
 # complete, clean data set
 
-#_____________________________________________________________________________
+# fit_and_scale(scaler, train, validate, test): takes in 
+
+#____________________________________________________________________________________
 
 # Required imports for these files:
 
@@ -21,10 +23,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+from scipy import stats
 import env
 import os
-
-#_____________________________________________________________________________
+import sklearn.preprocessing
+from sklearn.model_selection import train_test_split
+#____________________________________________________________________________________
 
 def get_connection(db, user=env.user, host=env.host, password=env.password):
     return f'mysql+pymysql://{user}:{password}@{host}/{db}'
@@ -52,7 +56,14 @@ def get_zillow_data(use_cache=True):
         , get_connection('zillow'))
         df.to_csv(filename, index=False)
         return df
-
+#__________________________________________________________________________________
+# outlier handling to remove quant_cols with >3.5 z-score (std dev)
+def remove_outliers(threshold, quant_cols, df):
+    z = np.abs((stats.zscore(df[quant_cols])))
+    df_without_outliers=  df[(z < threshold).all(axis=1)]
+    print(df.shape)
+    print(df_without_outliers.shape)
+    return df_without_outliers
 #__________________________________________________________________________________
 
 def prep_zillow(df):
@@ -69,17 +80,40 @@ def prep_zillow(df):
     # drop any duplicate rows
     df = df.drop_duplicates(keep='first')
     # convert column types from float to int
-    df = df.astype({'bedroomcnt': int, 'fips': int,
-          'calculatedfinishedsquarefeet': int, 'yearbuilt': int})
+    df = df.astype({'fips': int, 'parcelid': object})
     # remove homes with 0 BR/BD or SQ FT from the final df
     df = df[(df.bedroomcnt != 0) & (df.bathroomcnt != 0) & 
     (df.calculatedfinishedsquarefeet >= 69)]
+    # remove all rows where any column has z score gtr than 3
+    non_quants = ['fips', 'parcelid']
+    quants = df.drop(columns=non_quants).columns
+    # outlier handling
+    # remove numeric values with > 3.5 std dev
+    df = remove_outliers(3.5, quants, df)
 
     return df
-
 #__________________________________________________________________________________
 
 def wrangle_zillow():
     df = get_zillow_data()
     df = prep_zillow(df)
     return df
+
+#__________________________________________________________________________________
+
+#scaler = sklearn.preprocessing.MinMaxScaler()
+#scaler = sklearn.preprocessing.StandardScaler()
+#scaler = sklearn.preprocessing.RobustScaler()
+#pick the scaler and specify in the parameters
+
+
+def fit_and_scale(scaler, train, validate, test):
+    # only scales float columns
+    floats = train.select_dtypes(include='float64').columns
+    # fits scaler to training data only, then transforms
+    # train, validate & test
+    scaler.fit(train[floats])
+    scaled_train = pd.DataFrame(data=scaler.transform(train[floats]), columns=floats)
+    scaled_validate = pd.DataFrame(data=scaler.transform(validate[floats]), columns=floats)
+    scaled_test = pd.DataFrame(data=scaler.transform(test[floats]), columns=floats)
+    return scaled_train, scaled_validate, scaled_test
